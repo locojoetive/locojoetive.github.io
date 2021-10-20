36ddf23570fd81177c44643a4009a57386aafe8c
locojoetive <johannes.mic@gmail.com>
2021-10-20 08:57:29 +0200

    refactor js script

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
+
 // content container
 const aboutContainer = document.getElementById('about-container');
 const gamesContainer = document.getElementById('games-container');
 const cvContainer = document.getElementById('cv-container');
 
-// header
-const headerElement = document.getElementsByTagName('HEADER')[0];
 
 // header buttons
 const aboutButton = document.getElementById('about-button');
 const gamesButton = document.getElementById('games-button');
 const cvButton = document.getElementById('cv-button');
-activateHeaderButton(aboutButton);
 
-function activateHeaderButton(element)
+function highlightHeaderButton(element)
 {
-  const tablinks = document.getElementsByClassName('header-button');
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = '#1a1d45';
-    tablinks[i].style.color = '#ffffff';
+  const themeBackgroundColor = '#1a1d45';
+  const themeColor = '#ffffff';
+  for (let headerButton of document.getElementsByClassName('header-button')) {
+    headerButton.style.backgroundColor = themeBackgroundColor;
+    headerButton.style.color= themeColor;
   }
-  element.style.backgroundColor = '#ffffff';
-  element.style.color = '#1a1d45';
+  element.style.backgroundColor = themeColor;
+  element.style.color = themeBackgroundColor;
 }
 
 function openTab(tabName, elmnt) {
-  // activate right header button
-  activateHeaderButton(elmnt);
-
+  // header
+  const headerElement = document.getElementsByTagName('HEADER')[0];
+  // highlight respective header button
+  highlightHeaderButton(elmnt);
+  
   // scroll to the top
   if (tabName === 'about-container') {
     window.scroll(
@@ -38,9 +39,7 @@ function openTab(tabName, elmnt) {
   }
   // scroll to respective section
   else {
-    const headerHeight = headerElement.offsetHeight;
-    let goalPos  = document.getElementById(tabName).offsetTop - headerHeight;
-
+    const goalPos  = document.getElementById(tabName).offsetTop - headerElement.offsetHeight;
     // scroll to goalPos
     window.scroll(
       {
@@ -52,35 +51,39 @@ function openTab(tabName, elmnt) {
 }
 
 function hasScrolled() {
-  // activate right header button
+  // highlight respective header button
   const currentPos = document.documentElement.scrollTop;
   const halfWindowHeight = 0.5 * window.innerHeight;
   const gamesYPos = gamesContainer.offsetTop - halfWindowHeight;
   const cvYPos = cvContainer.offsetTop - halfWindowHeight;
   if (currentPos > cvYPos)
-    activateHeaderButton(cvButton);
+    highlightHeaderButton(cvButton);
   else if (currentPos > gamesYPos)
-    activateHeaderButton(gamesButton);
+    highlightHeaderButton(gamesButton);
   else
-    activateHeaderButton(aboutButton);  
+    highlightHeaderButton(aboutButton);  
 }
 
-window.addEventListener("scroll", (event) => {
-  hasScrolled();
-});
+function noScroll() {
+  // display overlay on mobile devices
+  window.scrollTo(0, 0);
+}
 
 function removeOverlay() {
   document.getElementById('overlay').style.display = "none";  
   window.removeEventListener('scroll', noScroll);
 }
 
+// start by highlighting about button
+highlightHeaderButton(aboutButton);
+
+// highlight button when scrolling
+window.addEventListener("scroll", hasScrolled);
+
+// display overlay on mobile devices
 const device = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-if(device) {
-  function noScroll() {
-    window.scrollTo(0, 0);
-  }  
+if(device) {  
   window.scrollTo(0, 0);
   window.addEventListener('scroll', noScroll);
-} else {
+} else
   removeOverlay();
-}
